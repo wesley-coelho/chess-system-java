6825702ac7e2d23e598b3146c3bb424d30a2ada9
Wesley Coelho <wesley_coelho2003@yahoo.com.br>
2024-06-23 14:47:12 -0300

    First Class Position

--- src/application/program.java
+++ src/application/program.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class program {
 
 	public static void main(String[] args) {
-		System.out.println("Helo World");
+
+
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
